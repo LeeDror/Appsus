9f828576a3b8ee535dd7788eaa541fd59205f0b8
Lee <leedror33@gmail.com>
2020-06-28 19:07:52 +0300

    שנםוא

--- js/pages/about.cmp.js
+++ js/pages/about.cmp.js
@@ -1,14 +1,49 @@
 export default {
   template: `
         <section class="about-us app-main" >
+          <div class="team-member flex justify-center">
+            <div class="tm-dit">
+              <img src="img/team/Ofra.jpg" alt="">
+              <h4>Ofra Bargad</h4>
+              <p class="text-dark">Web Development Student</p>
 
-
-          <div class="social-buttons">
-          <i class="fa fa-twitter"></i>
-              <a href="#"><i class="fa fa-twitter"></i></a>
-              <a href="#"><i class="fas fa-facebook"></i></a>
-              <a href="https://www.linkedin.com/in/lee-dror-145632138/" target="_blank"><i class="fa fa-linkedin"></i></a>
+              <div class="social-buttons flex justify-center">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://www.linkedin.com/in/lee-dror-145632138/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+              </div>
+            </div>
+            <div class="tm-desc flex align-center">
+              <p class="large text-dark  about-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+              eaque, laboriosam veritatis, quos non quis ad perspiciatis,
+              totam corporis ea, alias ut unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque,
+              laboriosam veritatis, quos non quis ad perspiciatis,
+              totam corporis ea, alias ut unde.</p>
+            </div>
           </div>
+          <hr>
+          <div class="team-member flex justify-center">
+          <div class="tm-dit">
+            <img src="img/team/Lee.jpg" alt="">
+            <h4>Lee Dror</h4>
+            <p class="text-dark">Web Development Student</p>
+
+            <div class="social-buttons flex justify-center">
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="https://www.linkedin.com/in/lee-dror-145632138/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+          </div>
+          <div class="tm-desc flex align-center">
+            <p class="large text-dark  about-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+            eaque, laboriosam veritatis, quos non quis ad perspiciatis,
+            totam corporis ea, alias ut unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque,
+            laboriosam veritatis, quos non quis ad perspiciatis,
+            totam corporis ea, alias ut unde.</p>
+          </div>
+        </div>
         </section>
     `,
 };
+
+
